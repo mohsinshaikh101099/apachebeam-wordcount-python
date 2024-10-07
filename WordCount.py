49d0185465a56217263fb045d1c465f6804d0e7c
Mohsin Shaikh <shaikhmohsin101099@gmail.com>
2024-10-07 20:51:47 +0530

    apache beam wor count python

---
+++ WordCount.py
@@ -0,0 +1,23 @@
+import apache_beam as beam
+
+with beam.Pipeline() as p:
+
+    # Read lines from input file
+
+    lines = p | 'Read' >> beam.io.ReadFromText('input.txt')
+
+    # Split each line into words and create a key-value pair (word, 1)
+
+    words = lines | 'ExtractWords' >> beam.FlatMap(lambda line: line.split())
+
+    # Count occurrences of each word
+
+    word_counts = words | 'CountWords' >> beam.combiners.Count.PerElement()
+
+    # Format the results into strings for output
+
+    formatted = word_counts | 'FormatResults' >> beam.Map(lambda word_count: f'{word_count[0]}: {word_count[1]}')
+
+    # Write the results to an output file
+
+    formatted | 'Write' >> beam.io.WriteToText('wordcounts.txt')
